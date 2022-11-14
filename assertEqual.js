81ac920d6e3e2bca5c1f4c0f3c6e824fccfb8f74
James Brown <jamesraymondbrown@gmail.com>
2022-11-14 23:03:33 +0000

    Adding assertEqual.js to my git

---
+++ assertEqual.js
@@ -0,0 +1,13 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log('🐕🐕🐕 Assertion Passed: ' + actual + ' === ' + expected);
+  } else {
+    console.log('🐍🐍🐍 Assertion Failed: ' + actual + ' !== ' + expected);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("Snail", "Snail");
+assertEqual(1, 100);
