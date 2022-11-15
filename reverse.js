1cc1dca2dc523d6562ec9c754d090b8ff8fe196a
James Brown <jamesraymondbrown@gmail.com>
2022-11-15 03:38:55 +0000

    This will reverse strings inputted into the command prompt. Type 'node reverse.js STRING STRING STRING STRING etc.' to reverse the strings

---
+++ reverse.js
@@ -0,0 +1,28 @@
+const args = process.argv;
+
+let reverser = function (args) {
+  let reversedChars = [];
+  let newArray = args.slice(2);
+    for (let i = 0; i < newArray.length; i++) {
+      let splitted = newArray[i];
+      let reversedString = '';
+      //console.log(splitted);
+        for (let j = splitted.length - 1; j >= 0; j--) {
+         reversedString += splitted[j];
+        }
+        reversedChars.push(reversedString);
+    }
+  for (let k = 0; k < reversedChars.length; k++) {
+    console.log(reversedChars[k]);
+  }
+}
+
+console.log(reverser(process.argv));
+
+//console.log(args.slice(2, 10));
+
+//console.log(yourFunction(process.argv[2]));
+
+//for (var j = args[i].length - 1; j >= 0; j--) {
+//  newString += args[i];
+//}
