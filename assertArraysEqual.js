926970e2cc281fa956a47e4f8b9690fa262e366c
James Brown <jamesraymondbrown@gmail.com>
2022-11-17 01:29:03 +0000

    fixing an issue in the eqArrays function (added a condition to compare array length)

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -8,6 +8,9 @@ const assertArraysEqual = function(array1, array2) {
 
 const eqArrays = function(array1, array2) {
   for (let i = 0; i < array1.length; i++) {
+    if (array1.length !== array2.length) {
+      return false;
+    }
     if (array1[i] !== array2[i]) {
       return false;
     }
