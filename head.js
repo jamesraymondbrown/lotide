733ff05904ac95c6f6509d32a1e53e8b72f27abc
James Brown <jamesraymondbrown@gmail.com>
2022-11-21 14:28:58 +0000

    made head modular

--- head.js
+++ head.js
@@ -1,16 +1,5 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`🐕🐕🐕 Assertion Passed: ' + ${actual} + ' === ' + ${expected}`);
-  } else {
-    console.log(`🐍🐍🐍 Assertion Failed: ${actual} !==  ${expected}`);
-  }
-};
-
 function head (array) {
   return array[0];
 }
 
-console.log(head([3]));
-
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports = head;
