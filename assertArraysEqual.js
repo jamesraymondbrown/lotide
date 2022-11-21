de4ca057291cd348f8f27d009992dfa68cf88048
James Brown <jamesraymondbrown@gmail.com>
2022-11-21 15:15:42 +0000

    making assertArraysEqual modular

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,23 +1,17 @@
+const eqArrays = require("./eqArrays");
+
 const assertArraysEqual = function(array1, array2) {
   if (eqArrays(array1, array2) === true) {
     console.log('🐕🐕🐕 Those two arrays are equal!');
   } else {
     console.log('🐍🐍🐍 Those two arrays are not equal!');
   }
-}
-
-const eqArrays = function(array1, array2) {
-  for (let i = 0; i < array1.length; i++) {
-    if (array1.length !== array2.length) {
-      return false;
-    }
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
 };
 
 //Test Scenarios
-console.log(assertArraysEqual([1, 2, 3], [1, 2, 3]));
-console.log(assertArraysEqual([1, 2, 3], [1, 2, '3']));
+// console.log(assertArraysEqual([1, 2, 3], [1, 2, 3]));
+// console.log(assertArraysEqual([1, 2, 3], [1, 2, '3']));
+
+//assertArraysEqual([4, 6, 9], [4, 6, 9]);
+
+module.exports = assertArraysEqual;
